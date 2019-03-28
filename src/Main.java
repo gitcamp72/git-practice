05400b83c986aba9931606bec5a98879eadadb3f
Nechemia Seif <nechemiaseif@gmail.com>
2019-03-27 23:35:04 -0400

    Remove template garbage!

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.util.Scanner;
-/**
- *
- * @author neche
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         String oneWrong = "wrong";
@@ -41,29 +30,3 @@ public class Main {
     }
 
 }
-
-
-/*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
- /*
-Ugh, I hate template garbage!
- */
